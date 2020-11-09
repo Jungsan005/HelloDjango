5ecdb0316836b46c8668728a130e09157d478db2
zzyzzy <zzyzzy@naver.com>
2020-11-09 14:59:37 +0900

    템플릿 설정 및 테스트

--- board/views.py
+++ board/views.py
@@ -2,6 +2,19 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello, Django Board!!")
+    return render(request, 'index.html')
 
+def list(request):
+    return render(request, 'board/list.html')
 
+def view(request):
+    return render(request, 'board/view.html')
+
+def write(request):
+    return render(request, 'board/write.html')
+
+def update(request):
+    return render(request, 'board/update.html')
+
+def delete(request):
+    return render(request, 'board/delete.html')
