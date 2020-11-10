78a9949230c16bc667de46279ea48916933747c8
zzyzzy <zzyzzy@naver.com>
2020-11-10 13:08:12 +0900

    django admin 적용

--- board/models.py
+++ board/models.py
@@ -10,3 +10,16 @@ class Board(models.Model):
     views = models.IntegerField(default=0)
     thumbup = models.IntegerField(default=0)
     contents = models.TextField()
+
+
+class Employee(models.Model):
+    fname = models.CharField(max_length=20)
+    lname = models.CharField(max_length=20)
+    email = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20)
+    hdate = models.DateField()
+    jobid = models.CharField(max_length=20)
+    sal = models.IntegerField()
+    commpct = models.FloatField()
+    mgrid = models.IntegerField()
+    deptid = models.IntegerField()
