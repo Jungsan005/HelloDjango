638679f5fdaa5466d642006414b7f7872400f988
zzyzzy <zzyzzy@naver.com>
2020-11-10 16:51:31 +0900

    django 뷰 적용

---
+++ board/forms.py
@@ -0,0 +1,15 @@
+from django.forms import ModelForm
+
+from board.models import Board
+
+
+class BoardForm(ModelForm):
+    class Meta:
+        model = Board
+        fields = ['title', 'userid', 'contents']
+
+        labels = {
+            'userid': '사용자명',
+            'title': '제목',
+            'contents': '내용',
+        }
